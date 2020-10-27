fa934f39ce470465e0d2e465bf5b5f13030ab56c
Carminati Alessio <alessioc3110@gmail.com>
2020-10-27 18:30:13 +0100

    Aggiunto titolo, risolti alcuni bug e sistemata grafica

--- src/app/lista-studenti/lista-studenti.component.ts
+++ src/app/lista-studenti/lista-studenti.component.ts
@@ -8,25 +8,58 @@ import { Component, OnInit } from '@angular/core';
 export class ListaStudentiComponent implements OnInit {
 
   student: string;
-  students: string[] = [];
+  studentList: string[] = [];
+
+  titleList: string[] = [
+    'Buongiorno',
+    'Hey.',
+    'Chi sarà il fortunato?',
+    "Pronti per l'interrogazione?"
+  ];
+  drawTitleList: string[] = [
+    'Oggi tocca a #',
+    'Forza #, tifiamo per te!',
+    '# è il fortunato vincitore'
+  ]
+  title: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.title = this.randomString(this.titleList);
+    let savedStudents = JSON.parse(localStorage.getItem('students'));
+    if(savedStudents)
+      this.studentList = savedStudents;
   }
 
   addStudent(): void{
-    this.students.push(this.student);
+    if(this.student){
+      this.studentList.push(this.student);
+      this.updateLocalStorage();
+    }
     this.student = null;
   }
 
   removeStudent(s): void{
-    this.students = this.students.filter(item => item !== s);
+    this.studentList = this.studentList.filter(item => item !== s);
+    this.updateLocalStorage();
   }
 
   drawStudent(){
-    var randStudent = this.students[Math.floor(Math.random() * this.students.length)];
+    let randStudent = this.randomString(this.studentList);
+    let randTitle = this.randomString(this.drawTitleList);
+    this.title = randTitle.replace('#', randStudent);
     this.removeStudent(randStudent);
+  }
+
+  //Utilities
+  randomString(array): string{
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  updateLocalStorage(){
+    localStorage.removeItem('students');
+    localStorage.setItem('students', JSON.stringify(this.studentList));
   }
 }
 
